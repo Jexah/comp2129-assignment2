c437cd66bfe1724b7bde0820d4600b7bb016f757
= <ijexah@gmail.com>
2015-04-17 21:03:54 +1000

    Command.c: *get_command_struct: Made third argument long by default

--- command.c
+++ command.c
@@ -36,6 +36,11 @@ static char *get_arg_malloc_ptr(char *input_ptr, int arg)
 	return get_arg_from_pointer_malloc_ptr(get_pointer_to_arg_ptr(input_ptr, arg));
 }
 
+static char *get_long_arg_malloc_ptr(char *input_ptr, int arg)
+{
+	return get_long_arg_from_pointer_malloc_ptr(get_pointer_to_arg_ptr(input_ptr, arg));
+}
+
 static int get_num_args(char *start_ptr)
 {
 	int num_args = (!!*start_ptr);
@@ -51,13 +56,8 @@ struct command_struct *get_command_struct(char *input_ptr)
 	if(!*input_ptr) return 0;
 	struct command_struct *malloc_ptr = calloc(sizeof(struct command_struct), 1);
 	int num_args = get_num_args(input_ptr);
-	for(int i = 0; i < (num_args < 3 ? num_args : 3); ++i)
-	{
-		malloc_ptr->args_malloc_ptr[i] = get_arg_malloc_ptr(input_ptr, i);
-	}
-	for(int i = 0; i < 3 - num_args; ++i)
-	{
-		malloc_ptr->args_malloc_ptr[i] = 0;
-	}
+	if(num_args >= 1) malloc_ptr->args_malloc_ptr[0] = get_arg_malloc_ptr(input_ptr, 0);
+	if(num_args >= 2) malloc_ptr->args_malloc_ptr[1] = get_arg_malloc_ptr(input_ptr, 1);
+	if(num_args >= 3) malloc_ptr->args_malloc_ptr[2] = get_long_arg_malloc_ptr(input_ptr, 2);
 	return malloc_ptr;
 }
