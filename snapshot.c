92130e95db3ed436f87842507d5d65c402e07a8e
= <ijexah@gmail.com>
2015-04-17 21:21:30 +1000

    Debug

--- snapshot.c
+++ snapshot.c
@@ -23,9 +23,9 @@ int main(void) {
 	{
 		struct command_struct *command = get_command_struct(buffer);
 		if(!command) continue;
-		if(command->args_malloc_ptr[0]) printf("%s", command->args_malloc_ptr[0]);
-		if(command->args_malloc_ptr[1]) printf("%s", command->args_malloc_ptr[1]);
-		if(command->args_malloc_ptr[2]) printf("%s", command->args_malloc_ptr[2]);
+		if(command->args_malloc_ptr[0]) printf("Arg1: %s\n", command->args_malloc_ptr[0]);
+		if(command->args_malloc_ptr[1]) printf("Arg2: %s\n", command->args_malloc_ptr[1]);
+		if(command->args_malloc_ptr[2]) printf("Arg3: %s\n", command->args_malloc_ptr[2]);
 	}
 
 	return 0;
