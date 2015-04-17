ef607a4acccb2d81b70e022906cc6c61bd283945
= <ijexah@gmail.com>
2015-04-17 23:00:34 +1000

    Debug

--- snapshot.c
+++ snapshot.c
@@ -10,40 +10,38 @@
 void print_help_string()
 {
 	printf(\
-		"\
-\	\	\	BYE clear database and exit
-\	\	\	HELP display this help message
-
-\	\	\	LIST KEYS displays all keys in current state
-\	\	\	LIST ENTRIES displays all entries in current state
-\	\	\	LIST SNAPSHOTS displays all snapshot ids
-
-\	\	\	GET <key> displays entry values
-\	\	\	DEL <key> deletes entry from current state
-\	\	\	PURGE <key> deletes entry from current state and snapshots
-
-\	\	\	SET <key> <value ...> sets entry values
-\	\	\	PUSH <key> <value ...> pushes each value to the front one at a time
-\	\	\	APPEND <key> <value ...> append each value to the back one at a time
-
-\	\	\	PICK <key> <index> displays entry value at index
-\	\	\	PLUCK <key> <index> displays and removes entry value at index
-\	\	\	POP <key> displays and removes the front entry value
-
-\	\	\	DROP <id> deletes snapshot
-\	\	\	ROLLBACK <id> restores to snapshot and deletes newer snapshots
-\	\	\	CHECKOUT <id> replaces current state with a copy of snapshot
-\	\	\	SNAPSHOT saves the current state as a snapshot
-
-\	\	\	MIN <key> displays minimum entry value
-\	\	\	MAX <key> displays maximum entry value
-\	\	\	SUM <key> displays sum of entry values
-\	\	\	LEN <key> displays number of entry values
-
-\	\	\	REV <key> reverses order of entry values
-\	\	\	UNIQ <key> removes repeated adjacent entry values
-\	\	\	SORT <key> sorts entry values in ascending order
-\	\	"
+		"BYE clear database and exit\n"\
+		"HELP display this help message\n"\
+		"\n"\
+		"LIST KEYS displays all keys in current state\n"\
+		"LIST ENTRIES displays all entries in current state\n"\
+		"LIST SNAPSHOTS displays all snapshot ids\n"\
+		"\n"\
+		"GET <key> displays entry values\n"\
+		"DEL <key> deletes entry from current state\n"\
+		"PURGE <key> deletes entry from current state and snapshots\n"\
+		"\n"\
+		"SET <key> <value ...> sets entry values\n"\
+		"PUSH <key> <value ...> pushes each value to the front one at a time\n"\
+		"APPEND <key> <value ...> append each value to the back one at a time\n"\
+		"\n"\
+		"PICK <key> <index> displays entry value at index\n"\
+		"PLUCK <key> <index> displays and removes entry value at index\n"\
+		"POP <key> displays and removes the front entry value\n"\
+		"\n"\
+		"DROP <id> deletes snapshot\n"\
+		"ROLLBACK <id> restores to snapshot and deletes newer snapshots\n"\
+		"CHECKOUT <id> replaces current state with a copy of snapshot\n"\
+		"SNAPSHOT saves the current state as a snapshot\n"\
+		"\n"\
+		"MIN <key> displays minimum entry value\n"\
+		"MAX <key> displays maximum entry value\n"\
+		"SUM <key> displays sum of entry values\n"\
+		"LEN <key> displays number of entry values\n"\
+		"\n"\
+		"REV <key> reverses order of entry values\n"\
+		"UNIQ <key> removes repeated adjacent entry values\n"\
+		"SORT <key> sorts entry values in ascending order\n"
 	);
 }
 
