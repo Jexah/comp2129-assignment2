c610dcb6b056e2942b9be7bc65d677ddee2a9138
= <ijexah@gmail.com>
2015-04-28 20:16:09 +1000

    Debug

--- snapshot.c
+++ snapshot.c
@@ -40,7 +40,7 @@ value *get_new_value(int number)
 STATUS *get_new_value(int number, value **new_value_ptr)
 {
     *new_value_ptr = calloc(sizeof(value), 1);
-	*new_value_ptr->value = number;
+	(*new_value_ptr)->value = number;
 	return new_value_ptr;
 }
 
